f1a038d1b9f5c12e058466b3fd13e7d730a0e36f
Vinicius Mendes <vinicius.ferrmendes@gmail.com>
2024-08-17 17:54:57 +0100

    connection with database

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = "mongodb+srv://root:root@bookstore.uq085.mongodb.net/?retryWrites=true&w=majority&appName=bookstore";
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,21 @@
 import express from "express";
-import { PORT } from "./config.js";
+import mongoose from "mongoose";
+import { PORT, mongoDBURL } from "./config.js";
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`App is listening to PORT ${PORT}.`);
+app.get("/", (req, res) => {
+    console.log(req);
+    return res.send("Hello World!");
+
 });
+
+
+mongoose.connect(mongoDBURL)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`App is listening to PORT ${PORT}.`);
+        });
+        console.log("App connected to database!");
+    })
+    .catch(error => console.log(error));
