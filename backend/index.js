5c06acbe7ac1bda5b1b44e6fcbf902128e9f7e5f
Vinicius Mendes <vinicius.ferrmendes@gmail.com>
2024-08-17 20:05:29 +0100

    Configuring CORS Policy

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import { PORT, mongoDBURL } from "./config.js";
 import booksRoute from "./routes/booksRoute.js";
 
@@ -7,6 +8,18 @@ const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS Policy
+
+// Option 1: Allow ALL Origins with Default of CORS
+// app.use(cors());
+
+// Option 2: Allow Custom Origins
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"]
+}));
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
