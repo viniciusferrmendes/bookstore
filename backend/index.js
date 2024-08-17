0feec21c438c8fbe9c90f73f4a6afa7dabb97ff4
Vinicius Mendes <vinicius.ferrmendes@gmail.com>
2024-08-17 19:25:51 +0100

    UPDATE - CRUD

--- backend/index.js
+++ backend/index.js
@@ -34,6 +34,7 @@ app.post("/books", async (req, res) => {
 
     } catch (error) {
         console.log(error);
+        res.status(500).send({ message: error.message });
     }
 });
 
@@ -59,6 +60,30 @@ app.get("/books/:id", async (req, res) => {
         const book = await Book.findOne({ _id: id });
 
         return res.status(200).json(book);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ message: error.message });
+    }
+});
+
+// --------------- UPDATE ---------------
+app.put("/books/:id", async (req, res) => {
+    try {
+        if (!req.body.title || !req.body.author || !req.body.publishYear) {
+            return res.status(400).send({
+                message: "Send all required fields: title, author and publish year."
+            });
+        }
+
+        const { id } = req.params;
+        const result = await Book.findByIdAndUpdate(id, req.body);
+
+        if (!result) {
+            return res.status(404).json({ message: "Book not found." });
+        }
+
+        return res.status(200).send({ message: "The book was updated with success!" });
 
     } catch (error) {
         console.log(error);
